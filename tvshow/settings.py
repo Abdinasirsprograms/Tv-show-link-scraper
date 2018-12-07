bfaa962e54415b8f24bb255cb2c7c41b7273460d
Abdinasir Noor <abdinasirnoor@outlook.com>
2018-12-07 12:01:01 -0500

    Change foldername (#3)
    * Delete settings.py
    
    * Delete pipelines.py
    
    * Delete middlewares.py
    
    * Delete items.py
    
    * Delete __init__.py
    
    * Delete watchseries.py
    
    * Delete __init__.py
    
    * Delete watchseries.cpython-37.pyc
    
    * Delete __init__.cpython-37.pyc
    
    * Delete settings.cpython-37.pyc
    
    * Delete __init__.cpython-37.pyc
    
    * Add files via upload
    
    * Delete startguide.html
    
    * Delete scrapy.cfg
    
    * Delete requirements.txt
    
    * Delete README.md
    
    * Delete LICENSE
    
    * Add files via upload
    
    * Delete raneming.txt
    
    * Create placeholder.txt
    
    * Add files via upload
    
    * Delete placeholder.txt
    
    * Create placeholder.txt
    
    * Add files via upload
    
    * Create placeholder.txt
    
    * Add files via upload
    
    * Delete placeholder.txt

--- tvshow/settings.py
+++ tvshow/settings.py
@@ -9,10 +9,10 @@
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'Tv show scraper'
+BOT_NAME = 'tvshow'
 
-SPIDER_MODULES = ['Tv show scraper.spiders']
-NEWSPIDER_MODULE = 'Tv show scraper.spiders'
+SPIDER_MODULES = ['tvshow.spiders']
+NEWSPIDER_MODULE = 'tvshow.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
